c806048ac9b52ea9a4f214f1ff8a6d95a0dd33ce
Varzk-Krethalen <varzk-krethalen@yahoo.co.uk>
2020-06-04 22:10:33 +0100

    Refactoring, adding GUI destination point

--- GeneticDynamicPathing/Assets/GridControl.cs
+++ GeneticDynamicPathing/Assets/GridControl.cs
@@ -8,23 +8,29 @@ public class GridControl : MonoBehaviour
     public Button gridButton;
     private Text inputText;
     private List<GameObject> GridPoints { get; set; }
+    private List<GameObject> GridLines { get; set; }
     private GameObject TemplatePoint { get; set; }
+    private GameObject TemplateDestination { get; set; }
+    private GameObject DestinationPoint { get; set; }
+    public int DimensionMax { get; set; }
+    public int Scale { get; set; } = 2;
 
     // Start is called before the first frame update
     void Start()
     {
         inputText = GameObject.FindWithTag("GridSize").GetComponent<InputField>().textComponent;
-        gridButton.onClick.AddListener(() => CreateGrid(2));
-        GetTemplatePoint();
+        gridButton.onClick.AddListener(() => CreateGrid());
+        GetTemplates();
     }
 
-    private void GetTemplatePoint()
+    private void GetTemplates()
     {
         TemplatePoint = GameObject.FindGameObjectWithTag("TemplatePoint");
+        TemplateDestination = GameObject.FindGameObjectWithTag("TemplateDestination");
     }
 
     //event listener for when the button is clicked
-    void CreateGrid(int scale)
+    void CreateGrid()
     {
         string sizeText = inputText.text; //TODO: Add size limit
         int gridRadius = int.Parse(sizeText);
@@ -40,14 +46,21 @@ public class GridControl : MonoBehaviour
                 {
                     if (!(x == 0 && y == 0 && z == 0))
                     {
-                        CreateGridPoint(x, y, z, scale);
+                        CreateGridPoint(x, y, z);
                     }
                 }
             }
         }
-        //if destination blank or invalid, default to random
-        //origin always 0,0,0?
-        //set origin to red, destination to green?
+        DimensionMax = gridRadius;
+        //if destination blank or invalid, default to random?
+    }
+
+    internal void CreateDestinationPoint(int x, int y, int z)
+    {
+        DestinationPoint = Instantiate(TemplateDestination);
+        Vector3 position = new Vector3(x * Scale, y * Scale, z * Scale);
+        DestinationPoint.transform.position = position;
+        DestinationPoint.transform.localScale = new Vector3((float)0.25, (float)0.25, (float)0.25);
     }
 
     private void DestroyGrid()
@@ -60,12 +73,32 @@ public class GridControl : MonoBehaviour
                 Destroy(gridPoint);
             }
         }
+        if (GridLines != null && GridLines.Count > 0)
+        {
+            for (int i = 0; i < GridLines.Count; i++)
+            {
+                GameObject gridLine = GridLines[i];
+                Destroy(gridLine);
+            }
+        }
+        if (DestinationPoint != null)
+        {
+            Destroy(DestinationPoint);
+            DestinationPoint = null;
+        }
     }
 
-    private void CreateGridPoint(int x, int y, int z, int scale)
+    internal void CreateGridLine(int startX, int startY, int startZ, int endX, int endY, int endZ)
+    {
+        //GameObject gridPoint = Instantiate(TemplateLine);
+
+        throw new NotImplementedException();
+    }
+
+    private void CreateGridPoint(int x, int y, int z)
     {
         GameObject gridPoint = Instantiate(TemplatePoint);
-        Vector3 position = new Vector3(x * scale, y * scale, z * scale);
+        Vector3 position = new Vector3(x * Scale, y * Scale, z * Scale);
         gridPoint.transform.position = position;
         gridPoint.transform.localScale = new Vector3((float)0.25, (float)0.25, (float)0.25);
         GridPoints.Add(gridPoint);
--- GeneticDynamicPathing/Assets/PathfinderDisplayScript.cs
+++ GeneticDynamicPathing/Assets/PathfinderDisplayScript.cs
@@ -2,43 +2,85 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DynamicPathfinder;
+using System;
 
 //[InitializeOnLoad]
 public class PathfinderDisplayScript : MonoBehaviour
 {
-    private static GeneticAlgorithm PathFinder { get; set; }
-    private static int x = 0;
-    static PathfinderDisplayScript()
+    public Button runButton;
+    private GeneticAlgorithm PathFinder { get; set; }
+    private GridControl GridController { get; set; }
+    public int NumberOfGenomes { get; set; } = 20;
+    public int IterationsPerGeneration { get; set; } = 20;
+    public int DestinationX { get; set; } = 5;
+    public int DestinationY { get; set; } = 5;
+    public int DestinationZ { get; set; } = 5;
+
+    private float iterationPeriod;
+    //private static DynamicPathfinder.get last path?
+    private bool running;
+
+    PathfinderDisplayScript()
     {
-        PathFinder = new GeneticAlgorithm(20, 20, new Coordinate(0,0,0), new Coordinate(5,5,5));
-        //Create grid, define start
-        //Create the objects here!
-        //EditorApplication.update += Update;
+        runButton.onClick.AddListener(() => RunPathfinder());
     }
 
-    //call a function for starting a new population?
-    //should use it for resetting path & destination
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void RunPathfinder()
     {
-        
+        if (!running)
+        {
+            if (GridController == null)
+            {
+                GetGridController();
+            }
+            UpdateSettings();
+            PathFinder = new GeneticAlgorithm(NumberOfGenomes, IterationsPerGeneration, new Coordinate(0, 0, 0), new Coordinate(DestinationX, DestinationY, DestinationZ));
+            GridController.CreateDestinationPoint(DestinationX, DestinationY, DestinationZ);
+        }
+        running = !running;
     }
 
-    // Update is called once per frame
-    static void Update() //add time limit mechanism? Else we get 60+ iterations a second
+    private void GetGridController()
     {
-        //run iteration, show selected genome's path on screen
-        //if iteration > x, new generation
-        //if generation > y, wait for input
+        GridController = FindObjectOfType(typeof(GridControl)) as GridControl;
     }
 
-    public void ButtonTest(string blah)
+    private void UpdateSettings()
     {
-        Text text = transform.Find("Text").GetComponent<Text>();
-        text.text = blah + x;
-        x++;
+        //get iteration period, genomes, etc
+        NumberOfGenomes = 0;
+        IterationsPerGeneration = 0;
+        DestinationX = 0;
+        DestinationY = 0;
+        DestinationZ = 0;
+        throw new NotImplementedException();
+    }
+
+    private float elapsedTime = 0.0f;
+    public float period = 1.1f;
+    void Update()
+    {
+        if (running)
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime > period)
+            {
+                elapsedTime = 0.0f;
+                RunIteration();
+                DisplayIteration();
+            }
+        }
+    }
+
+    private void RunIteration()
+    {
+        PathFinder.Update();
+    }
+
+    private void DisplayIteration()
+    {
+        int startX, startY, startZ, endX, endY, endZ;
+        startX = startY = startZ = endX = endY = endZ = 0;
+        GridController.CreateGridLine(startX, startY, startZ, endX, endY, endZ);
     }
 }
-//create a dll for algorithm
